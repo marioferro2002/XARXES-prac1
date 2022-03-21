c4d345d10d34d9d2ea528b5504e52753e9221b72
mariofernandezrodriguez <marioferro2002@gmail.com>
2022-03-21 16:03:06 +0100

    READ FINISHED

--- client.c
+++ client.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <time.h>
+
 #define MAX_CHAR 51 //El tamany del array es tan gran a causa de la linea d'elements
+#define T 1
+#define U 2
+#define N 8
+#define O 3
+#define P 2
+#define Q 4
 
+//Tipus de paquets
 
-//Funcions main
-int check_debug_mode(int argc, char *argv[]);
+#define REG_REQ 0xa0
+#define REG_ACK 0xa1
+#define REG_NACK 0xa2
+#define REG_REJ 0xa3
+#define REG_INFO 0xa4
+#define INFO_ACK 0xa5
+#define INFO_NACK 0xa6
+#define INFO_REJ 0xa7
 
-//Funcions registre
-struct config_info read_config_files(int argc, char *argv[], int debug_mode);
-char *get_configname(int argc, char *argv[]);
+//Estat del client/servidor
 
+#define DISCONNECTED 0xf0
+#define NOT_REGISTERED 0xf1
+#define WAIT_ACK_REG 0xf2
+#define WAIT_INFO 0xf3
+#define WAIT_ACK_INFO 0xf4
+#define REGISTERED 0xf5
+#define SEND_ALIVE 0xf6
+int status = DISCONNECTED;
 
 struct config_info {
     unsigned char id[11];
@@ -28,18 +52,98 @@ struct config_pdu_udp {
 };
 
 
+//Funcions main
+int check_debug_mode(int argc, char *argv[]);
+void print_start_status(struct config_info info);
+void show_status(int status);
+
+//Funcions registre
+struct config_info read_config_files(int argc, char *argv[], int debug_mode);
+char *get_configname(int argc, char *argv[]);
+struct config_pdu_udp client_to_pdu (struct config_info config);
+int register_client_connection (struct config_info client, int debugmode);
+unsigned char* create_pack_udp(struct config_pdu_udp udp_pack, char type);
+void wait_reg_status(int status);
+
+
 
 int main(int argc, char *argv[]) {
 
     int debug_status;
     debug_status = check_debug_mode(argc,argv);
-
     printf("mode debug: %i \n",debug_status);
 
 
     struct config_info user = read_config_files(argc, argv, debug_status);
+    print_start_status(user);
+    int connect = register_client_connection(user, debug_status);
+
+}
 
 
+
+
+int register_client_connection (struct config_info client, int debugmode){
+
+    int init_socket;
+    //Creem socket
+    init_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (init_socket < 0){
+        printf("Error al crear Socket %i", init_socket);
+        exit(-1);
+    }
+
+    //Creem constructors y variables necessaries per al bind i comunicació amb el server
+    struct sockaddr_in sck_addr_reg;
+    sck_addr_reg.sin_addr.s_addr = htonl(0);
+    sck_addr_reg.sin_port = htonl(0);
+    sck_addr_reg.sin_family = AF_INET;
+
+
+    if( bind(init_socket,(struct sockaddr*)&sck_addr_reg, sizeof(struct sockaddr)) != 0 ){
+        printf("connexio dolenta");
+        exit(-1);
+    }
+
+    //Preparem tos els paquets per al registre i començem amb l'estat WAIT_REG
+
+    struct config_pdu_udp reg_str = client_to_pdu(client);
+    status = NOT_REGISTERED;
+    wait_reg_status(status);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*------------------FUNCIONS GENERALS----------------------------*/
+
+struct config_pdu_udp client_to_pdu (struct config_info config){
+
+    struct config_pdu_udp reg_pack;
+    strcpy((char *) reg_pack.id_transm,(const char *) config.id);
+    strcpy((char *) reg_pack.id_comm,"0000000000");
+    strcpy(reg_pack.dades, "sdddddd");
+
+    return reg_pack;
 }
 
 struct config_info read_config_files(int argc, char *argv[], int debug_mode) {
@@ -65,14 +169,15 @@ struct config_info read_config_files(int argc, char *argv[], int debug_mode) {
     cpy_token = strtok(cpy_token, ";");
 
     int i = 0;
-    while (cpy_token != NULL && i != 5){
+    while (cpy_token != NULL){
 
         strcpy((char *) client.elements[i], cpy_token);
         cpy_token = strtok(NULL, ";");
-        printf("%s %i \n",client.elements[i], i);
         i++;
     }
-
+    for(int x = i; x < 6; x++){
+        strcpy((char *) client.elements[i], "NULL");
+    }
 
 
 
@@ -106,7 +211,7 @@ struct config_info read_config_files(int argc, char *argv[], int debug_mode) {
 
 
     if(debug_mode == 1){
-        printf("Configuració del client guardada!!\n Id: %s \n Elements: %s ...\n Local_TCP: %s \n Server: %s \n Server_UDP: %s \n", client.id, client.elements[1], client.local_TCP, client.server, client.server_udp);
+        //printf("Configuració del client guardada!!\n Id: %s \n Elements: %s ...\n Local_TCP: %s \n Server: %s \n Server_UDP: %s \n", client.id, client.elements[1], client.local_TCP, client.server, client.server_udp);
     }
     return client;
 }
@@ -119,6 +224,8 @@ int check_debug_mode(int argc,char *argv[]){
     }
     return 0;
 }
+
+
 char *get_configname(int argc, char *argv[]){
     for(int i = 1; i < argc;i++){
         if(strcmp(argv[i], "-c") == 0){
@@ -127,13 +234,60 @@ char *get_configname(int argc, char *argv[]){
     }
     return "client.cfg";
 }
+void print_start_status(struct config_info info) {
+    printf("********************* DADES DISPOSITIU ***********************\n");
+    printf("Identificador: %s\n", info.id);
+    printf("Estat: DISCONNECTED\n\n");
+    printf(" Param      valor\n");
+    printf(" -----      --------\n");
+    int i = 0;
 
-/*char* decode_type(char type_code){
-    if(strcmp(type_code, (const char *) 'oxao')){
-        return 'n';
+   while(strcmp((char *)info.elements[i],"NULL") != 0){
+           printf("%s       None\n", info.elements[i]);
+       i++;
+   }
+    printf("***************************************************************\n");
+
+
+}
+
+void show_status(int status) {
+
+    switch(status){
+        case 0xf0:
+            printf("DISCONNECTED\n");
+            break;
+        case 0xf1:
+            printf("NOT_REGISTERED\n");
+            break;
+        case 0xf2:
+            printf("WAIT_ACK_REG\n");
+            break;
+        case 0xf3:
+            printf("WAIT_INFO\n");
+            break;
+        case 0xf4:
+            printf("WAIT_ACK_INFO\n");
+            break;
+        case 0xf5:
+            printf("REGISTERED\n");
+            break;
+        case 0xf6:
+            printf("SEND_ALIVE\n");
+            break;
     }
 }
- */
+
+void wait_reg_status(int status){
+
+    printf("Dispositiu passa a l'estat: ");
+    show_status(status);
+
+}
+
+
+
+
 
 
 
