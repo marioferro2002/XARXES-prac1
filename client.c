512550ef683ab24a0ac29a953c8f039812750886
mariofernandezrodriguez <marioferro2002@gmail.com>
2022-03-24 10:53:54 +0100

    FASE REGISTER FETa

--- client.c
+++ client.c
@@ -5,6 +5,8 @@
 #include <netinet/in.h>
 #include <time.h>
 #include <netdb.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #define MAX_CHAR 51 //El tamany del array es tan gran a causa de la linea d'elements
 #define T 1
@@ -38,7 +40,7 @@
 
 struct config_info {
     unsigned char id[11];
-    unsigned char elements[6][16];
+    unsigned char elements[6][8];
     unsigned char local_TCP[6];
     unsigned char server[13];
     unsigned char server_udp[6];
@@ -66,18 +68,19 @@ struct socket_pck sock;
 //Funcions main
 int check_debug_mode(int argc, char *argv[]);
 void print_start_status(struct config_info info);
-void show_status(int status);
+void show_status(unsigned char state);
 
 //Funcions registre
 struct config_info read_config_files(int argc, char *argv[], int debug_mode);
 char *get_configname(int argc, char *argv[]);
 struct config_pdu_udp client_to_pdu (struct config_info config);
 void register_client_connection (struct config_info client, int debugmode);
-unsigned char* create_pack_udp(struct config_pdu_udp udp_pack, char type);
-void wait_reg_status(int status);
+void wait_reg_status(unsigned char status);
 void udp_socket(struct config_info client);
 void send_udp_package(struct config_info client, unsigned char type);
-
+struct config_pdu_udp recv_pck_udp();
+void wait_ack_reg_phase(unsigned char type, struct  config_pdu_udp tcp_pck, struct config_info user);
+struct config_pdu_udp ack_reg_packmaker (struct config_pdu_udp tcp_package, struct config_info user);
 
 int main(int argc, char *argv[]) {
 
@@ -87,6 +90,7 @@ int main(int argc, char *argv[]) {
 
 
     struct config_info user = read_config_files(argc, argv, debug_status);
+
     print_start_status(user);
     register_client_connection(user, debug_status);
 
@@ -98,17 +102,45 @@ int main(int argc, char *argv[]) {
 void register_client_connection (struct config_info client, int debugmode){
 
     udp_socket(client);
-    send_udp_package(client,REG_REQ);
-    struct config_pdu_udp reg_server;
-    recvfrom(sock.udp_socket, (void *)&reg_server, sizeof(struct config_pdu_udp), 0, (struct sockaddr*) 0,(socklen_t *) 0);
+    int unfinished_register_try = 0;
 
+
+    while (unfinished_register_try < O){
+
+        for(int i = 0; i < P; i++) {
+            struct config_pdu_udp pck_recieved;
+            send_udp_package(client,REG_REQ);
+            status = WAIT_ACK_REG;
+            wait_reg_status(status);
+            pck_recieved = recv_pck_udp();
+
+            if (pck_recieved.type == REG_ACK && status == WAIT_ACK_REG){
+                wait_ack_reg_phase(REG_INFO, pck_recieved, client);
+                return;
+            }else if (pck_recieved.type == REG_NACK){
+                show_status(REG_NACK);
+            }else if (pck_recieved.type == REG_REJ){
+                show_status(REG_REJ);
+            }else if (pck_recieved.type == INFO_NACK){
+                show_status(INFO_NACK);
+            }
+            else{
+                printf("paquet inesperat");
+                status = NOT_REGISTERED;
+                unfinished_register_try++;
+            }
+
+        }
+        sleep(5);
+
+        unfinished_register_try++;
+    }
 
 }
 void udp_socket(struct config_info client){
     struct sockaddr_in sck_addr_reg;
     struct hostent *ent;
 
-    //Creem socket
     sock.udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock.udp_socket < 0){
         printf("Error al crear Socket %i", sock.udp_socket);
@@ -127,8 +159,7 @@ void udp_socket(struct config_info client){
     }
 
 
-    ent = gethostbyname("localhost");
-    printf("%s\n", ent);
+    ent = gethostbyname((const char*) client.server);
     if(!ent)
     {
         printf("Error! No trobat: %s \n",client.server);
@@ -137,7 +168,7 @@ void udp_socket(struct config_info client){
 
     memset(&sock.udp_addr, 0, sizeof(struct sockaddr_in));
     sock.udp_addr.sin_addr.s_addr = (((struct in_addr *)ent->h_addr_list[0])->s_addr);
-    sock.udp_addr.sin_port = htons(atoi("2022"));
+    sock.udp_addr.sin_port = htons(atoi((const char *)&client.server_udp ));
     sock.udp_addr.sin_family = AF_INET;
 
 
@@ -146,18 +177,77 @@ void udp_socket(struct config_info client){
 void send_udp_package(struct config_info client, unsigned char type){
     struct config_pdu_udp reg_str = client_to_pdu(client);
     reg_str.type = type;
-
     int bytes_send = 0;
+
+
     bytes_send = sendto(sock.udp_socket,(void *)&reg_str,sizeof(reg_str), 0, (const struct sockaddr*)&sock.udp_addr, sizeof(sock.udp_addr));
+
     if(bytes_send != 84){
         printf("tamañ no coincideix\n");
     }
 
+}
+
+struct config_pdu_udp recv_pck_udp(){
+    struct config_pdu_udp reg_server;
+    int a = 0;
+    a = recvfrom(sock.udp_socket, (void *)&reg_server, sizeof(struct config_pdu_udp), 0, (struct sockaddr*) 0,(socklen_t *) 0);
+    if(a < 0){
+        printf("No s'ha rebut cap paquet per UDP");
+        exit(1);
+    }
+    return reg_server;
+}
+
+void wait_ack_reg_phase(unsigned char type, struct  config_pdu_udp tcp_pck, struct config_info user){
+
+    struct sockaddr_in addr_tcp_port = sock.udp_addr;
+    unsigned char server_port[6];
+    int bytes_send = 0;
+
+    tcp_pck.type = REG_INFO;
+    show_status(tcp_pck.type);
+
+    //Creem copia de la addreça i canviem port
+    strcpy((char *)server_port, tcp_pck.dades);
+    tcp_pck = ack_reg_packmaker(tcp_pck, user);
+    addr_tcp_port.sin_port = htons(atoi((const char*)&server_port));
 
 
+    bytes_send = sendto(sock.udp_socket,(void *)&tcp_pck,sizeof(tcp_pck), 0, (const struct sockaddr*)&addr_tcp_port, sizeof(addr_tcp_port));
+
+    if (bytes_send != 84){
+        exit(1);
+    }
+    struct config_pdu_udp info_ack_packet;
+
+    sleep(2 * T);
+
+
+    show_status(info_ack_packet.type);
 }
 
 
+struct config_pdu_udp ack_reg_packmaker (struct config_pdu_udp tcp_package, struct config_info user){
+    strcpy((char *)&tcp_package.id_transm, (const char*)&user.id);
+    strcpy((char *)&tcp_package.dades[0], (const char*)&user.local_TCP);
+    strcpy((char *)&tcp_package.dades[4], (const char*) ",");
+    int i = 0;
+    int pos = 5;
+
+    while (strcmp((char *)&user.elements[i],"NULL") != 0 && i < 5){
+        strcpy((char *)&tcp_package.dades[pos], (const char*)&user.elements[i]);
+
+        if(strcmp((char *)&user.elements[i + 1],"NULL") != 0 && i < 4){
+            strcpy((char *)&tcp_package.dades[pos + 7], (const char *) ";");
+        }
+        pos += 8;
+        i++;
+    }
+
+
+    return tcp_package;
+}
 
 
 
@@ -293,9 +383,9 @@ void print_start_status(struct config_info info) {
 
 }
 
-void show_status(int status) {
+void show_status(unsigned char state) {
 
-    switch(status){
+    switch(state){
         case 0xf0:
             printf("DISCONNECTED\n");
             break;
@@ -314,13 +404,34 @@ void show_status(int status) {
         case 0xf5:
             printf("REGISTERED\n");
             break;
-        case 0xf6:
-            printf("SEND_ALIVE\n");
+        case 0xa0:
+            printf("REG_REQ \n");
+            break;
+        case 0xa1:
+            printf("REG_ACK \n");
+            break;
+        case 0xa2:
+            printf("REG_NACK \n");
+            break;
+        case 0xa3:
+            printf("REG_REJ\n");
+            break;
+        case 0xa4:
+            printf("REG_INFO\n");
+            break;
+        case 0xa5:
+            printf("INFO_ACK\n");
+            break;
+        case 0xa6:
+            printf("INFO_NACK\n");
+            break;
+        case 0xa7:
+            printf("INFO_REJ\n");
             break;
     }
 }
 
-void wait_reg_status(int status){
+void wait_reg_status(unsigned char status){
     int h, m, s;
     time_t time_now = time(NULL);
     struct tm *tm_struct = localtime(&time_now);
